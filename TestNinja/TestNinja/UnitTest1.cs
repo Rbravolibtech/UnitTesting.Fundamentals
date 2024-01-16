bc3cfae51967f69d2e39bca6dfa55ba1e4078074
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-16 11:58:30 -0700

    deleted files installed new packages

---
+++ TestNinja/TestNinja/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja;
+
+[TestClass]
+public class ReservationTests
+{ 
+    [TestMethod]
+    public void CancelledBy_UserIsAdmin_ReturnsTrue()
+    {
+
+        //Arrange
+        var reservation = new Reservation();
+
+        //Act
+
+      var result =   reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+        //Assert
+
+        //this is how u do a unit test below
+
+        Assert.IsTrue(result);
+    }
+}
