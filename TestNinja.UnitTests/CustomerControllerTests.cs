cb2a1f06aea2d38d09be66e0902bab602ef1d087
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-17 12:55:10 -0700

    created a test for customer and action to get the result i want to see if code has no bugs

--- TestNinja.UnitTests/CustomerControllerTests.cs
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -12,18 +12,37 @@ namespace TestNinja.UnitTests
             var controller = new CustomerController();
 
             var result = controller.GetCustomer(0);
-            
-            // NotFound 
+
+            //NOT FOUND
             Assert.That(result, Is.TypeOf<NotFound>());
-            
-            // NotFound or one of its derivatives 
-//            Assert.That(result, Is.InstanceOf<NotFound>());
+
+
+            //NOT FOUND OR ONE OF THE DERIVATIVE 
+            //Assert.That(result, Is.InstanceOf<NotFound
         }
 
+        //PRACTICE TEST BELOW TOP IS EXAMPLE 
         [Test]
         public void GetCustomer_IdIsNotZero_ReturnOk()
         {
-            
+            // Arrange
+
+            //Arrange section, you create an instance of the CustomerController class.
+            //This is the controller that you'll be testing.
+            var controller = new CustomerController();
+
+            // Act
+
+            //The Act section involves calling a method or performing an action that you want to test.
+            var result = controller.GetCustomer(1);
+
+            // Assert
+
+            //This is where you check if the behavior of the method matches your expectations.
+            Assert.That(result, Is.TypeOf<RetryAttribute>());
+
+
         }
+
     }
 }
--- TestNinja.UnitTests/HtmlFormatterTests.cs
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -3,18 +3,37 @@ using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
+
+    //[TestFixture]: Indicates that the HtmlFormatterTests class is a test fixture containing unit tests
     [TestFixture]
     public class HtmlFormatterTests
     {
+
+        //[Test]: Marks the method
+        //FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement as a unit test.
         [Test]
         public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
         {
+
+            //Creates an instance of the HtmlFormatter class.
             var formatter = new HtmlFormatter();
 
             var result = formatter.FormatAsBold("Abc");
 
             // Specific
             Assert.That(result, Is.EqualTo("<stong>abc</strong>"));
+
+            //more general
+
+            //Specific Assertion: Checks if the result is exactly equal to
+
+            Assert.That(result, Does.StartWith("<strong>"));
+
+            Assert.That(result, Does.EndWith("</strong>"));
+
+            Assert.That(result, Does.Contain("abc"));
+
+
             
         }
         
