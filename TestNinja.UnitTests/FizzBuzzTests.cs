d98e826765360c9da7967a67be27157e9d9afc58
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-17 15:19:50 -0700

    verify the behavior of the FizzBuzz.GetOutput method for different input scenarios,

--- TestNinja.UnitTests/FizzBuzzTests.cs
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -6,27 +6,34 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class FizzBuzzTests
     {
+
+        //Test is what you will test on method
         [Test]
+
+        //This is a test method named 
         public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
         {
-            var result = FizzBuzz.GetOutput(15);
-            
-            Assert.That(result, Is.EqualTo("FizzBuzz"));
+
+            //method of the FizzBuzz class with the input value 15.
+            var result =   FizzBuzz.GetOutput(15);
+
+            //This line performs an assertion to verify that the result obtained from calling fizzbuzz
+            Assert.That(result, Is.EqualTo("FIzzBuzz"));
         }
         
         [Test]
         public void GetOutput_InputIsDivisibleBy3Only_ReturnFizz()
         {
             var result = FizzBuzz.GetOutput(3);
-            
-            Assert.That(result, Is.EqualTo("Fizz"));
+
+            Assert.That(result, Is.EqualTo("FIzz"));
         }
         
         [Test]
         public void GetOutput_InputIsDivisibleBy5Only_ReturnBuzz()
         {
             var result = FizzBuzz.GetOutput(5);
-            
+
             Assert.That(result, Is.EqualTo("Buzz"));
         }
         
@@ -34,7 +41,7 @@ namespace TestNinja.UnitTests
         public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber()
         {
             var result = FizzBuzz.GetOutput(1);
-            
+
             Assert.That(result, Is.EqualTo("1"));
         }
         
