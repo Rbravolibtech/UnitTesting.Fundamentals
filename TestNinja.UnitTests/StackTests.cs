757f8083aab8ade67793724899425437e21e66aa
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-22 12:27:04 -0700

    checking the behavior of the Pop method on a Stack<string>

--- TestNinja.UnitTests/StackTests.cs
+++ TestNinja.UnitTests/StackTests.cs
@@ -23,9 +23,105 @@ namespace TestNinja.UnitTests
 
             stack.Push("a");
 
-            Assert.That(Stack.Count, Is.EqualTo(1));
+            Assert.That(stack.Count, Is.EqualTo(1));
 
         }
-                   
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_ReturnObjectOnTheTop()
+        {
+            //Arrange
+            var Stack = new Stack<string>();
+            Stack.Push("a");
+            Stack.Push("b");
+            Stack.Push("c");
+
+            //Act
+            var result = Stack.Pop();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_RemoveObjectOnTheTop()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            stack.Pop();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperation()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            var result = stack.Pop();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_ReturnObjectOnTopOfTheStack()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            var result = stack.Pop();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_DoesNotRemoveTheObjectOnTopOfTheStack()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+         stack.Pop();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo("3"));
         }
     }
+ }
