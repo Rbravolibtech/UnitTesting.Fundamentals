8cdff3ad27c2aebaebdd428875bf6ca191041216
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-23 13:27:51 -0700

    added comments to book repository

--- TestNinja/Mocking/BookingRepository.cs
+++ TestNinja/Mocking/BookingRepository.cs
@@ -2,21 +2,25 @@
 
 namespace TestNinja.Mocking
 {
+    // Interface defining the contract for a booking repository
     public interface IBookingRepository
     {
         IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null);
     }
 
+    // Implementation of the IBookingRepository interface
     public class BookingRepository : IBookingRepository
     {
         public IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null)
         {
+            // Creating a new UnitOfWork instance for database interaction
             var unitOfWork = new UnitOfWork();
+                // Querying the Booking entities from the database
             var bookings =
                 unitOfWork.Query<Booking>()
                     .Where(
                         b =>  b.Status != "Cancelled");
-
+            // If excludedBookingId is provided, further filter out bookings with that specific ID
             if (excludedBookingId.HasValue)
                 bookings = bookings.Where(b => b.Id != excludedBookingId.Value);
 
