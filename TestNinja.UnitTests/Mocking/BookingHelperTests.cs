790904f8eee259356f0d5f1508058bad947e3388
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-24 11:13:46 -0700

    booking and getting result of time and date

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -8,141 +8,43 @@ using TestNinja.Mocking;
 namespace TestNinja.UnitTests.Mocking
 {
     [TestFixture]
-    public class BookingHelper_OverlappingBookingsExistTests
+    public class BookingHelper_OverlappingBookingExistTests
     {
-        private Booking _existingBooking;
-        private Mock<IBookingRepository> _repository;
-
-        [SetUp]
-        public void SetUp()
-        {
-            _existingBooking = new Booking
-            {
-                Id = 2,
-                ArrivalDate = ArriveOn(2017, 1, 15),
-                DepartureDate = DepartOn(2017, 1, 20),
-                Reference = "a"
-            };
-            
-            _repository = new Mock<IBookingRepository>();
-            _repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
-            {
-                _existingBooking
-
-            }.AsQueryable());            
-        }
-
         [Test]
-        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        public void BookingStartsAndFinishesBeforeAnExisitngBooking_ReturnEmptyString()
         {
-            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            var repository = new Mock<IBookingRepository>();
+            repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
+                {
+                new Booking
+                {
+                    ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
+                    DepartureDate = new DateTime(2017, 1, 20, 10, 0, 0),
+                    Reference = "a"
+                }
+                //The .AsQueryable() is used to convert the List<Booking> into an IQueryable<Booking
+                
+            }.AsQueryable());
+
+           var result =  BookingHelper.OverlappingBookingsExist(new Booking
             {
                 Id = 1,
-                ArrivalDate = Before(_existingBooking.ArrivalDate, days: 2),
-                DepartureDate = Before(_existingBooking.ArrivalDate)
-            }, _repository.Object);
-            
+                ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
+                DepartureDate = new DateTime(2017, 1, 14, 10, 0, 0),
+            }, repository.Object);
+
             Assert.That(result, Is.Empty);
         }
 
-        [Test]
-        public void BookingStartsBeforeAndFinishesInTheMiddleOfAnExistingBooking_ReturnExistingBookingsReference()
-        {
-            var result = BookingHelper.OverlappingBookingsExist(new Booking
-            {
-                Id = 1,
-                ArrivalDate = Before(_existingBooking.ArrivalDate),
-                DepartureDate = After(_existingBooking.ArrivalDate)
-            }, _repository.Object);
-            
-            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
-        }
-        
-        [Test]
-        public void BookingStartsBeforeAndFinishesAfterAnExistingBooking_ReturnExistingBookingsReference()
-        {
-            var result = BookingHelper.OverlappingBookingsExist(new Booking
-            {
-                Id = 1,
-                ArrivalDate = Before(_existingBooking.ArrivalDate),
-                DepartureDate = After(_existingBooking.DepartureDate)
-            }, _repository.Object);
-            
-            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
-        }
-
-        [Test]
-        public void BookingStartsAndFinishesInTheMiddleOfAnExistingBooking_ReturnExistingBookingsReference()
-        {
-            var result = BookingHelper.OverlappingBookingsExist(new Booking
-            {
-                Id = 1,
-                ArrivalDate = After(_existingBooking.ArrivalDate),
-                DepartureDate = Before(_existingBooking.DepartureDate)
-            }, _repository.Object);
-            
-            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
-        }
-
-        [Test]
-        public void BookingStartsInTheMiddleOfAnExistingBookingButFinishesAfter_ReturnExistingBookingsReference()
-        {
-            var result = BookingHelper.OverlappingBookingsExist(new Booking
-            {
-                Id = 1,
-                ArrivalDate = After(_existingBooking.ArrivalDate),
-                DepartureDate = After(_existingBooking.DepartureDate)
-            }, _repository.Object);
-            
-            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
-        }
-
-        [Test]
-        public void BookingStartsAndFinishesAfterAnExistingBooking_ReturnEmptyString()
-        {
-            var result = BookingHelper.OverlappingBookingsExist(new Booking
-            {
-                Id = 1,
-                ArrivalDate = After(_existingBooking.DepartureDate),
-                DepartureDate = After(_existingBooking.DepartureDate, days: 2)
-            }, _repository.Object);
-            
-            Assert.That(result, Is.Empty);
-        }
-        
-        [Test]
-        public void BookingsOverlapButNewBookingIsCancelled_ReturnEmptyString()
-        {
-            var result = BookingHelper.OverlappingBookingsExist(new Booking
-            {
-                Id = 1,
-                ArrivalDate = After(_existingBooking.ArrivalDate),
-                DepartureDate = After(_existingBooking.DepartureDate),
-                Status = "Cancelled"
-            }, _repository.Object);
-            
-            Assert.That(result, Is.Empty);
-        }        
-
-        private DateTime Before(DateTime dateTime, int days = 1)
-        {
-            return dateTime.AddDays(-days);
-        }
-
-        private DateTime After(DateTime dateTime, int days = 1)
-        {
-            return dateTime.AddDays(days);
-        }
-
-        private DateTime ArriveOn(int year, int month, int day)
-        {
-            return new DateTime(year, month, day, 14, 0, 0);
-        }
-
-        private DateTime DepartOn(int year, int month, int day)
-        {
-            return new DateTime(year, month, day, 10, 0, 0);
-        }
-        
     }
+
+        
 }
+
+
+
+
+
+    
+
+  
