88ad42575468907963fddcf6158f2017614be28c
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-24 13:07:44 -0700

    refractoring and making several test

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -10,30 +10,47 @@ namespace TestNinja.UnitTests.Mocking
     [TestFixture]
     public class BookingHelper_OverlappingBookingExistTests
     {
+        private Booking _booking;
+        private Mock<IBookingRepository> _repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _booking = new Booking
+            {
+                ArrivalDate = ArriveOn(2017, 1, 15),
+                DepartureDate = DepartOn(2017, 1, 20),
+                Reference = "a"
+            };
+
+             _repository = new Mock<IBookingRepository>();
+            _repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
+                {
+                _booking
+
+                //The .AsQueryable() is used to convert the List<Booking> into an IQueryable<Booking
+
+            }.AsQueryable());
+        }
+
         [Test]
         public void BookingStartsAndFinishesBeforeAnExisitngBooking_ReturnEmptyString()
         {
-            var repository = new Mock<IBookingRepository>();
-            repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
-                {
-                new Booking
-                {
-                    ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
-                    DepartureDate = new DateTime(2017, 1, 20, 10, 0, 0),
-                    Reference = "a"
-                }
-                //The .AsQueryable() is used to convert the List<Booking> into an IQueryable<Booking
-                
-            }.AsQueryable());
+            
 
            var result =  BookingHelper.OverlappingBookingsExist(new Booking
             {
                 Id = 1,
-                ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
-                DepartureDate = new DateTime(2017, 1, 14, 10, 0, 0),
-            }, repository.Object);
+                ArrivalDate = ArriveOn(2017, 1, 15),
+                DepartureDate = DepartOn(2017, 1, 14),
+            }, _repository.Object);
 
             Assert.That(result, Is.Empty);
+        }
+
+        private DateTime ArriveOn(int year, int month, int day)
+        {
+            return new DateTime(year, month, day, 14, 0, 0);
         }
 
     }
